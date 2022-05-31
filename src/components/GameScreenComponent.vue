ef367d20aaa577d829a6ee34d9dfbaa79da1ee40
Evgeny <hatber.index@gmail.com>
2022-06-01 05:09:36 +0900

    Было принято решение делать всё на 'canvas'. Лишние компоненты удалены

---
+++ src/components/GameScreenComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='game-screen-component vue-component'>
+    <canvas class="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'game-screen-component',
+  components: {},
+  data: function () {
+    return {}
+  },
+  props: [],
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style scoped='scoped' lang='sass'>
+.game-screen-component
+  height: 100%
+  width: 100%
+</style>
